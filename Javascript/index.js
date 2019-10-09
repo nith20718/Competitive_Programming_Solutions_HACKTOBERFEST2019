5815413c4012790eedab7195e8edfb6a7f8483f4
Aman Prateek <42955585+amanprateek123@users.noreply.github.com>
2019-10-09 14:40:15 +0530

    Create index.js

---
+++ Javascript/index.js
@@ -0,0 +1,12 @@
+<html>
+<body>
+<div>
+<button id="btn" onClick="hello">Submit</button>
+</div>
+</body>
+<script>
+function hello() {
+  document.getElementById("btn").style.backgroundColor = "red";
+}
+</script>
+</html>
